aa15d37acde2e43b9b7cc47d38ce81304c922bb2
Florent <mertens.florent00@gmail.com>
2025-11-05 20:19:58 +0100

    update

--- main.js
+++ main.js
@@ -1,1 +1,22 @@
-console.log('Hello World!');
+const addTaskBtn = document.getElementById("add-task-button");
+const overlayModal = document.getElementById("overlay-modal");
+const cancelBtn = document.getElementById("cancel-button");
+const saveBtn = document.getElementById("save-button");
+const form = document.getElementById("form");
+
+addTaskBtn.addEventListener("click", function () {
+  overlayModal.classList.add("open");
+  addTaskBtn.disabled = true;
+});
+
+cancelBtn.addEventListener("click", function () {
+  overlayModal.classList.remove("open");
+  addTaskBtn.disabled = false;
+  form.reset();
+});
+
+saveBtn.addEventListener("click", function () {
+  overlayModal.classList.remove("open");
+  addTaskBtn.disabled = false;
+  form.reset();
+});
